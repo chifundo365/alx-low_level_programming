326d050589776f3300c25afdcd2a5dcf94e48961
= <chifundobiziweck@gmail.com>
2023-02-16 17:57:47 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 /**
- *Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
- */
+ *main - use the printf function
+ *
+ *Description: using the main function
+ *prints "with proper grammer, but the outcome is a piece of art"
+ * Return: 0
+*/
 int main(void)
-{printf("/"with proper grammar, but the outcome is a piece of art");
+{printf("\"with proper grammar, but the outcome is a piece of art");
 return (0);
 }
