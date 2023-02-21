4965ddf2e14d0d15e150f5a384f2ccc68c4170f8
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-20 21:29:53 -0800

    added task 0 file and code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main : entry function;
+ *
+ * return: return 0
+ *
+ *
+ **/
+int _putchar(char c)
+{
+  printf("%c \n", c);
+}
+
+int main(void)
+{
+  _putchar('c');
+
+  return 0;
+
+}
+
