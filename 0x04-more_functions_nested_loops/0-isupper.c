a8e51e85ca5d14b321ef3df82fc0abac018bf901
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-23 12:04:42 -0800

    task 0 is upper file addede

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main"
+
+/**
+*int _isupper(int c): checks if numbers is uppercase
+*main: entry file
+*Return: Always 0
+*/
+
+int _isupper(int c)
+{
+return _isupper(int c);
+}
