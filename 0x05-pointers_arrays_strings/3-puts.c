40a5aa6e427653119ab1daf653dc6669dfe0e177
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-27 22:20:31 -0800

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*description: contains a function that prints string to stdout
+*_puts - prints a string
+*@str - string to print
+*/
+
+void _puts(char *str)
+{
+int i;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+}
