572a9f7c2785bd78c899852f1023b5eeb1b1c628
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-27 18:45:53 -0800

    configured task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 /**
 *swap_int - swaps values of two integers
-*@a:int pointer type
-*@b:int pointer type
+*@a: int pointer type
+*@b: int pointer type
 */
 void swap_int(int *a, int *b)
-{
-int temp;
-temp = *a; 
+{int temp;
+temp = *a;
 *a = *b;
 *b = temp;
 }
