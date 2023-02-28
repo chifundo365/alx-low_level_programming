96b8677282f70cf6c4dfcb53f1b648ba3a686330
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-27 17:20:48 -0800

    configured task 0 file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 
 /**
 * void reset_to_98(int *n) : a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+* @*n : a pointer that updates the variable passed to the function
 */
-
 
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
