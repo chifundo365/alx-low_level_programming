f8df841aaf1c4806fdf4858c071e1c044727a1d2
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-27 22:47:21 -0800

    corrected task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,10 +8,12 @@
 
 void _puts(char *str)
 {
-int i;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
-}
+
