bd36594e8bf360a1043486412a94d31788d283af
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-27 17:42:39 -0800

    configured task 0/

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
-
 /**
-*Description: a  function that updates the parameter n to 98 
-*void reset_to_98(int *n) : set pointer parameter ro  98.
+*Description: a  function that updates the parameter n to 98
+*reset_to_98(int *n) : set pointer parameter ro  98.
 * @n : a pointer parameter
 */
-
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
