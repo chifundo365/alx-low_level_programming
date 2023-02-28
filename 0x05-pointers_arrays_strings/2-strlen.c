5ee8170fa8943a0388f3c9c4fd71512be73ea25c
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-02-27 21:33:16 -0800

    added task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strlen - returns length of a string
+*@c : string charecter
+*/
+
+int _strlen(char *s)
+{
+int counter = 0;
+while(*(s++)
+{
+counter++;
+}
+return counter;
+}
