bb91bd74e6e9ddf0db6e69ef0d44afe625e6f543
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-03-08 17:43:32 -0800

    added tASK 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
 * _strlen_recursion - returns the lenght of a string using recursion
 *@s: string to return length
-*Return - string length;
+*Return: string length;
 */
 
 int _strlen_recursion(char *s)
@@ -12,5 +12,5 @@ if (*s == '\0')
 {
 return (0);
 }
-return (1 + _strlen_recursion(s 	+ 1));
+return (1 + _strlen_recursion(s + 1));
 }
