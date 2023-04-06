045b3ae1c897baaa3d5453f99e07b44162c1efc3
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-04-06 16:30:38 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list.
+ * @head: listint_t lists to be freed
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+while (*head != NULL)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+}
+
+}
