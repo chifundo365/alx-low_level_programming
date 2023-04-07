ddba10b496bdf8a13e53cbed56e9374f4b51a203
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-04-07 08:59:44 -0700

    fixed task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *temp;
 unsigned int current = 0;
 
+if (head == NULL)
+{
+return (NULL);
+}
+
 while (head != NULL)
 {
 temp  = head;
