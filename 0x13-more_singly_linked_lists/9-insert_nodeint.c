dc5cb040ee3a8e75d5d4f417aa609fe88a94b1b1
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-04-09 18:06:29 -0700

    another way

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,48 +6,46 @@
  * @head: pointer to a pointer to the head of the list
  * @idx: index of the list where the new node should be added
  * @n: value to store in the new node
- *
  * Return: address of the new node, or NULL if it failed
  **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *prev_node, *current_node;
+listint_t *newNode, *prevNode, *currentNode;
 unsigned int i;
 
 if (head == NULL)
-{ return (NULL); }
-
-new_node = malloc(sizeof(*new_node));
-if (new_node == NULL)
-{ return (NULL); }
-
-new_node->n = n;
-
-if (idx == 0)
 {
-new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
-
-prev_node = NULL;
-current_node = *head;
-
-for (i = 0; i < idx && current_node != NULL; i++)
-{
-prev_node = current_node;
-current_node = current_node->next;
-}
-
-if (i != idx)
-{
-free(new_node);
 return (NULL);
 }
 
-new_node->next = current_node;
-prev_node->next = new_node;
+newNode = (listint_t *)malloc(sizeof(listint_t *));
+if (newNode == NULL)
+{
+return (NULL);
+}
+newNode->n = n;
+if (idx == 0)
+{
+newNode->next = *head;
+*head = newNode;
+return (newNode);
+}
+prevNode = NULL;
+currentNode = *head;
 
-return (new_node);
+for (i = 0; i < idx && currentNode != NULL; i++)
+{
+prevNode = currentNode;
+currentNode = currentNode->next;
+}
+if (i != idx)
+{
+free(newNode);
+return (NULL);
 }
 
+newNode->next  = currentNode;
+prevNode->next = newNode;
+
+return (newNode);
+}
