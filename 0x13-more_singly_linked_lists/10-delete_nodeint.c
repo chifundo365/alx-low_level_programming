d4305d49d59050deb777838b4e601144cef24510
Chifundo Biziweck <chifundobiziweck@gmail.com>
2023-04-10 03:48:21 -0700

    task 10 done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,26 +11,37 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *currentNode;
+listint_t  *currentNode, *previousNode;
 unsigned int i;
-if (*head == NULL)
-{ return (-1); }
 
-for (i = 0; i < index && *head != NULL; i++)
-{
-currentNode = *head;
-*head = (*head)->next;
-}
-
-if (i == index)
-{
-free(currentNode);
-*head = NULL;
-return (1);
-}
-else
+if (head == NULL || *head == NULL)
 {
 return (-1);
 }
 
+previousNode = NULL;
+currentNode = *head;
+
+for (i = 0; i < index && currentNode != NULL; i++)
+{
+previousNode = currentNode;
+currentNode = currentNode->next;
 }
+
+if (currentNode == NULL)
+{ return (-1); }
+
+if (previousNode == NULL)
+{
+*head = currentNode->next;
+}
+else
+{
+previousNode->next = currentNode->next;
+}
+
+free(currentNode);
+return (1);
+
+}
+
