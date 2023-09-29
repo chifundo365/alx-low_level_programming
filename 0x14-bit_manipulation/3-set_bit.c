378ee0b4e845a3e15aeb534ed4be3bd62a935286
= <=>
2023-09-28 19:07:34 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit to 1
+ * @n: pointer to integer
+ * @index: index
+ * Return: 1 or -1 if failed
+ * Description: set a bit at specified index
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(*n) * 8 - 1)
+	{
+		return (-1);
+	}
+
+	*n |= (1UL << index);
+	return (1); 
+}
+
