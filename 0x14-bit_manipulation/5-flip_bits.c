34d724a18463e6a1c8de831a6630b1e8420329aa
= <=>
2023-09-29 02:07:19 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * flip_bits - find the number if bits to flip
+ * @n: integer 1
+ * @m: integer 2
+ * Return: the number of bit to flip
+ * Description: finds and returns the number of bits to
+ * flip from 1 number to another
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int diff = 0;
+
+	while (n != 0 || m != 0)
+	{
+		diff += ((n & 1LU) & (m & 1LU));
+
+		if (n != 0)
+		{
+			n <<= 1LU;
+		}
+
+		if (m != 0)
+		{
+			m <<= 1LU;
+		}
+
+	}
+
+	return (diff);
+}
