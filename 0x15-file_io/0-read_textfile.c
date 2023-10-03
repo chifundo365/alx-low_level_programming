be38b957b090734c40c09c5e83d4916b7ee620c9
= <=>
2023-10-03 07:23:43 -0700

    task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,46 +11,51 @@
  * Description: prints letters or return 0 if fails to read or write
  */
 
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t read_bytes = 0;
-	int fd;
-	char *buffer = malloc(letters + 1);
+	int fd = 0;
+	size_t read_bytes = 0;
+	size_t written_bytes = 0;
+	char *buffer = NULL;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
 	{
+		close(fd);
+		return (0);
+	}
+
+	read_bytes = read(fd, buffer, letters);
+
+	if (read_bytes == 0)
+	{
+		close(fd);
+		free(buffer);
+
+		return (0);
+	}
+
+	written_bytes = write(STDIN_FILENO, buffer, read_bytes);
+	if (written_bytes == 0 || written_bytes != read_bytes)
+	{
+		close(fd);
 		free(buffer);
 		return (0);
 	}
 
-
-	if (filename == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd > -1)
-	{
-		read_bytes = read(fd, buffer, letters);
-
-		if (read_bytes == -1)
-		{
-			close(fd);
-			free(buffer);
-
-			return (0);
-		}
-
-		buffer[letters] = '\0';
-
-		write(STDOUT_FILENO, buffer, letters);
-	}
-
-	close(fd);
 	free(buffer);
-	return (read_bytes);
+	close(fd);
+
+	return (written_bytes);
 }
+
