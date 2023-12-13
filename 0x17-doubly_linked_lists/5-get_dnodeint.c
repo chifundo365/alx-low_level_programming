6dd2b62f882db5295eeab5c937fb43fe9d91991b
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-13 04:38:05 -0800

    task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns a node at specified index
+ * @head: pointer to the first node
+ * @index: the index
+ * Return: address of node at specified index or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	if (!head || index < 0)
+		return (0);
+
+	while (head)
+	{
+		if (index == i)
+			return (head);
+		i++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
