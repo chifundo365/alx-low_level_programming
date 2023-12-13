8890de554c67abc8f13ecf814aae185a208a34bf
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-13 04:24:20 -0800

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - freez a d-linked list
+ * head: pointer to the first node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
