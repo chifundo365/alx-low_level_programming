1f6c44003f3690adb9ffb413a8b6e7ce3c1b37aa
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-13 03:25:05 -0800

    taks 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - find length of a double linked list
+ * @h: pointer to the first node
+ * Return: length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int x = 0;
+
+	while(h = h->next)
+		x++;
+	return (x);
+}
