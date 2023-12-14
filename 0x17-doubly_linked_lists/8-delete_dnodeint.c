87e6209323aeebc84baa3524b0b82bd64f61eb41
chifundo biziweck <chifundobiziweck@gmail.com>
2023-12-14 07:38:37 -0800

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at index
+ * @head: pointer to a pointer to first node
+ * @index: index
+ * Return: 1 or -1 if fails
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int x = 1;
+	dlistint_t *del, *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		return (1);
+	}
+
+	while(temp)
+	{
+		if (x == index)
+		{
+			del = temp->next;
+			temp->next = temp->next->next;
+			if (temp->next->next)
+			{
+				temp->next->next->prev = temp;
+			}
+			else
+			{
+				temp->next = NULL;
+			}
+
+			free(del);
+			return (1);
+		}
+
+		temp = temp->next;
+		x++;
+	}
+
+	return (-1);
+}
